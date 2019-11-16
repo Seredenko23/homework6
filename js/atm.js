7b1852f3019da1846c9ddf92878f5cfd46836f00
unknown <srvladyk555@gmail.com>
2019-11-16 15:24:01 +0200

    added simple ui, withdraw and showBalance work correctly

--- js/atm.js
+++ js/atm.js
@@ -1,9 +1,15 @@
 class Atm {
-  constructor (atmWrapper, options) {
+  constructor (atmWrapper) {
     this.atmWrapper = atmWrapper,
-    this.options = options,
-    this.info = document.getElementById('info'),
+    this.controlElements = {
+      options: atmWrapper.getElementsByClassName('options-js')[0],
+      add: atmWrapper.getElementsByClassName('add-js')[0],
+      total: atmWrapper.getElementsByClassName('add-js')[1],
+      withdraw: atmWrapper.getElementsByClassName('withdraw-js')[0]
+    }
+    this.info = atmWrapper.getElementsByClassName('atm-info-js')[0],
     this.balance = 0,
+    this.tempBalance = 0,
     this.values = {
       1000: 0,
       500: 0,
@@ -37,7 +43,7 @@ class Atm {
 
   withdrawCash (card, value) {
     if (value > this.balance) {
-      console.log('There is not enough money!')
+      this.info.textContent = 'В банкоматі не вистачає грошей!'
     } else {
       let temp = value
       const bills = {}
@@ -54,28 +60,73 @@ class Atm {
         })
         this.balance -= value
         card.addMoney(value)
+        this.info.textContent = 'Гроші успішно перераховані на вашу карту'
+        this.hideExcept('options')
       } else {
-        console.log('This don`t enough bills!')
+        this.info.textContent = 'Не вистачає купюр!'
       }
     }
   }
 
+  addHandler (event) {
+    const target = event.target
+    if (target.dataset.values) {
+      const total = atmWrapper.getElementsByClassName('total')[0]
+      this.tempBalance += +target.dataset.values
+      total.textContent = this.tempBalance
+    }
+  }
+
+  withdrawHandler (event) {
+    const target = event.target
+    if (target.dataset.type === 'withdraw') {
+      const input = atmWrapper.getElementsByClassName('withdraw-input')[0]
+      this.withdrawCash(card, input.value)
+    }
+  }
+
+  toMain () {
+    this.info.textContent = 'Оберіть дію'
+    this.hideExcept('options')
+  }
+
   optionHandler (event) {
     const target = event.target
+    this.hideExcept(target.dataset.type)
     switch (target.dataset.type) {
       case 'add':
-        info.textContent = 'TO DO'
+        this.info.textContent = 'Скільки грошей бажаете додати?'
         break
       case 'withdraw':
-        info.textContent = 'TO DO'
+        this.info.textContent = 'Яку сумму хочете зняти?'
         break
       case 'balance':
-        info.textContent = `На рахунку ${this.balance}`
+        this.info.textContent = `На рахунку ${this.balance}`
+        break
+      default:
+        this.hideExcept('options')
         break
     }
   }
 
+  hideExcept (elem) {
+    const elements = Object.values(this.controlElements)
+    elements.forEach((el) => {
+      if (el.classList.contains(elem + '-js')) {
+        el.style.display = 'flex'
+      } else {
+        el.style.display = 'none'
+      }
+    })
+  }
+
   initEvents () {
-    this.options.addEventListener('click', this.optionHandler.bind(this))
+    const main = atmWrapper.getElementsByClassName('main-js')[0]
+    const withdraw = atmWrapper.getElementsByClassName('withdraw-js')[0]
+    const add = atmWrapper.getElementsByClassName('add-js')[1]
+    this.controlElements.options.addEventListener('click', this.optionHandler.bind(this))
+    main.addEventListener('click', this.toMain.bind(this))
+    withdraw.addEventListener('click', this.withdrawHandler.bind(this))
+    add.addEventListener('click', this.addHandler.bind(this))
   }
 }
